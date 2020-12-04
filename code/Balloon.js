fd5b8c83bbabf119ba3b06ab923d7a5e37270dc2
kevinlopez8554 <kevinlopez8554@csu.fullerton.edu>
2020-12-03 22:48:51 -0800

    added automatic balloon creation for N balloons

--- code/Balloon.js
+++ code/Balloon.js
@@ -1,4 +1,4 @@
-class Balloon{
+class Balloon {
     constructor(balloonNumber, D, P, V) {
         this.number = balloonNumber;
         this.d = D;
@@ -6,21 +6,64 @@ class Balloon{
         this.v = V;
     }
 
-    getNumber(){
+    getNumber() {
         return this.number;
     }
-    setDPV(dpv){
+
+    setDPV(dpv) {
         //code
     }
-    getDPV(){
+
+    getDPV() {
         return this.d + " " + this.p + " " + this.v;
     }
 
-    printDPV(){
-    return "D:" + this.d + " \n" +  "P:" + this.p + " \n" + "V:" + this.v;
+    printDPV() {
+        return "D:" + this.d + " \n" + "P:" + this.p + " \n" + "V:" + this.v;
     }
-    moveAll(){
+
+    //moveFromAtoB(a,b){
+        //a is what we take from
+        // b is what we give to
+        //
+    //}
+
+
+
+    getFrom(){
+
+    }
+
+
+    giveTo(){
+
+    }
+    moveAll() {
         //move
+        //let's go with the truly random method
+        let a = getRandomArbitrary(1, 3);//it will generate a number between 1 and 3
+        let b = getRandomArbitrary(1,3);// we need to make sure that the numbers arent the same in the end
+        do{ // while a and b are equal do it
+            a = getRandomArbitrary(1, 3);
+            b = getRandomArbitrary(1, 3);
+        }while(a === b)
+        // D = 1, p = 2, v = 3
+        if (a === 1) {// while a === 1 give to b
+            while(this.d > 0){
+
+            }
+
+        } else if (a === 2) {// while a === 2 give to b
+
+        } else if (a === 3) {// while a === 3 give to b
+
+        }
     }
+
+
+    getRandomArbitrary(min, max) {
+        return Math.round(Math.random() * (max - min) + min);
+    }
+
     //shift?
 }
